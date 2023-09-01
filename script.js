1034be9c2d18d29b16257106b0753cd861fe9e6c
Victoria <victorialors@hotmail.com>
2023-08-31 19:57:23 -0500

    Create operate function that passes 2 numbers and operator

--- script.js
+++ script.js
@@ -3,20 +3,35 @@ let firstNumber
 let secondNumber
 let operator
 
+function operate (firstNumber, secondNumber, operator) {
+    if (operator == "+") {
+        return(add(firstNumber, secondNumber))
+    }
+
+    else if (operator == "-") {
+        return subtract(firstNumber, secondNumber)
+    }
+    else if (operator == "*") {
+        return multiply(firstNumber, secondNumber)
+    }
+
+    if (operator == "/") {
+        return divide(firstNumber, secondNumber)
+    }
+}
+
+console.log(operate(3, 1, "/"))
+
 // Functions for basic math operators
 function add(n1, n2) {
-    return n1 + n2
-}
+    return n1 + n2}
 
 function subtract(n1, n2) {
-    return n1 - n2
-}
+    return n1 - n2}
 
 function multiply (n1, n2) {
-    return n1 * n2
-}
+    return n1 * n2}
 
 function divide (n1, n2) {
-    return n1 / n2
-}
+    return n1 / n2}
 
